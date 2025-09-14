3feffbb9b58caf4c2a8d45bd6f77eb828bc3ce6e
Diego Garcia <tu_correo_de_github@example.com>
2025-09-14 16:11:41 -0300

    se creó el modelo address

--- src/models/address.model.js
+++ src/models/address.model.js
@@ -1,1 +1,25 @@
 //modelo direcciones
+import {Schema, model} from "mongoose";
+
+const addressSchema = new Schema(
+    {
+        street:{
+            type:String,
+            required: true
+        },
+        number:{
+            type:Number,
+            required: true
+        },
+        city:{
+            type:String,
+            required: true
+        },
+        country:{
+            type:String,
+            required: true
+        }
+    }
+);
+
+export const AddressModel = model("Address", addressSchema)
